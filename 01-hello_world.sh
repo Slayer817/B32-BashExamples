eb4f2d0413e69de72d27f606948d83077a330c71
Nelson <nelson182@live.ca>
2020-11-20 02:40:13 -0500

    echo-01 termine

--- 01-hello_world.sh
+++ 01-hello_world.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 
+echo "salut Antoine"
+echo Salut antoine
+echo 'Alut Santoine'
+
+echo -n "Hello "
+echo "World"
+
+echo -e "1\n2\n3\n4" #-e interprete les metacaracteres
+
+read -p "My name is?" nom #read = cin, -p "mon message" >> variable
+echo "Le nom est: " $nom
